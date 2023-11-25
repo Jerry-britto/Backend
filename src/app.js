e2bb91c3d6040e5f0a587c2e79c2e2adea806838
Jerry Britto <jbritto2004@gmail.com>
2023-11-25 19:24:18 +0530

    professional setup for routers and controllers

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.json({ limit: "16kb" })); //setting the limit for json data
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //nesting of objects
 app.use(express.static("path")); //Storing assets(images,etc) in a public folder
 app.use(cookieParser()); //to set or get cookies on the browser and also perform CRUD operations
+
+//import router
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
 export { app };
